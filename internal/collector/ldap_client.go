6cf7a8d1a55331e9382704b178918dff7514d763
wangqing <wangqing@uniontech.com>
2025-11-18 11:38:20 +0800

    添加更多指标
    Signed-off-by: wangqing <wangqing@uniontech.com>

--- internal/collector/ldap_client.go
+++ internal/collector/ldap_client.go
@@ -81,9 +81,11 @@ func (c *LDAPClient) SearchCount(baseDN, filter string) (int, error) {
 
 	res, err := c.conn.Search(req)
 	if err != nil {
+		c.logger.Debugf("SearchCount failed for baseDN=%s, filter=%s: %v", baseDN, filter, err)
 		return 0, err
 	}
 
+	c.logger.Debugf("SearchCount returned %d entries for baseDN=%s, filter=%s", len(res.Entries), baseDN, filter)
 	return len(res.Entries), nil
 }
 
@@ -103,13 +105,17 @@ func (c *LDAPClient) SearchMonitor(dn, attr string) (string, error) {
 
 	res, err := c.conn.Search(req)
 	if err != nil || len(res.Entries) == 0 {
+		c.logger.Debugf("SearchMonitor failed for dn=%s, attr=%s: %v", dn, attr, err)
 		return "", err
 	}
 
 	vals := res.Entries[0].GetAttributeValues(attr)
 	if len(vals) == 0 {
-		return "", fmt.Errorf("attribute %s not found in %s", attr, dn)
+		err := fmt.Errorf("attribute %s not found in %s", attr, dn)
+		c.logger.Debugf("SearchMonitor failed: %v", err)
+		return "", err
 	}
 
+	c.logger.Debugf("SearchMonitor returned value %s for dn=%s, attr=%s", vals[0], dn, attr)
 	return vals[0], nil
 }
